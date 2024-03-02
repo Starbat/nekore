4ca3d0116b47689334174e2fd2ceb3401081e23e
Starbat <45294825+Starbat@users.noreply.github.com>
2024-03-02 14:47:13 +0100

    test: add test_privileged_invoices_by_issuer

--- tests/models/test_invoice_collection.py
+++ tests/models/test_invoice_collection.py
@@ -19,3 +19,19 @@ def test_privileged_invoices(
     result: Final = collection.privileged_invoices
 
     assert tuple(result) == (privileged_invoice,)
+
+
+def test_privileged_invoices_by_issuer(
+    dummy_allocation_strategy: AllocationStrategy,
+    privileged_invoice: Invoice,
+    unprivileged_invoice: Invoice,
+) -> None:
+    collection: Final = InvoiceCollection(
+        "test_collection",
+        [privileged_invoice, unprivileged_invoice],
+        dummy_allocation_strategy,
+    )
+
+    result: Final = collection.privileged_invoices_by_issuer
+
+    assert result == {privileged_invoice.issuer_name: [privileged_invoice]}
