9e6dd2a00a114f59a8ad9999c9e3d41d7326fb3b
Starbat <45294825+Starbat@users.noreply.github.com>
2024-03-10 01:46:47 +0100

    test: Apartment prevents overlapping tenant periods

--- tests/models/test_apartment.py
+++ tests/models/test_apartment.py
@@ -1,10 +1,12 @@
 import datetime as dt
 from decimal import Decimal
 
+import pytest
+
 from nekore.models import Apartment, Contact, Tenant, TimePeriod
 
 
-def test_validate_tenant_periods() -> None:
+def test_validate_tenant_periods_allows_not_overlapping() -> None:
     t1 = Tenant(
         contact=Contact("Tenant 1", "Street", "1", "12345", "city"),
         number_of_people=1,
@@ -22,3 +24,24 @@ def test_validate_tenant_periods() -> None:
         ),
     )
     Apartment(floor_space=Decimal(50), tenants=[t1, t2])
+
+
+def test_validate_tenant_periods_raises_error_if_overlapping() -> None:
+    t1 = Tenant(
+        contact=Contact("Tenant 1", "Street", "1", "12345", "city"),
+        number_of_people=1,
+        period=TimePeriod(
+            start=dt.date.fromisoformat("2020-01-01"),
+            end=dt.date.fromisoformat("2020-01-10"),
+        ),
+    )
+    t2 = Tenant(
+        contact=Contact("Tenant 1", "Street", "1", "12345", "city"),
+        number_of_people=1,
+        period=TimePeriod(
+            start=t1.period.start - dt.timedelta(days=1),
+            end=t1.period.start + dt.timedelta(days=1),
+        ),
+    )
+    with pytest.raises(ValueError):
+        Apartment(floor_space=Decimal(50), tenants=[t1, t2])
