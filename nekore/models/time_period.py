fe8220c36fee4863055d91ef75decbd18fd73cd9
Starbat <45294825+Starbat@users.noreply.github.com>
2024-03-10 01:38:56 +0100

    style: use type hint Self

--- nekore/models/time_period.py
+++ nekore/models/time_period.py
@@ -1,6 +1,6 @@
 import datetime as dt
 from dataclasses import dataclass, field
-from typing import Union
+from typing import Self
 
 
 @dataclass(frozen=True, eq=True, slots=True)
@@ -18,18 +18,18 @@ class TimePeriod:
             self, "duration", self.end - self.start + dt.timedelta(days=1)
         )
 
-    def __contains__(self, other: Union[dt.date, "TimePeriod"]) -> bool:
+    def __contains__(self, other: dt.date | Self) -> bool:
         if isinstance(other, dt.date):
             return self.start <= other <= self.end
         return self.start <= other.start <= other.end <= self.end
 
-    def is_overlapping_lower_range(self, other: "TimePeriod") -> bool:
+    def is_overlapping_lower_range(self, other: Self) -> bool:
         return other.start in self and other.end not in self
 
-    def is_overlapping_upper_range(self, other: "TimePeriod") -> bool:
+    def is_overlapping_upper_range(self, other: Self) -> bool:
         return other.end in self and other.start not in self
 
-    def intersection(self, other: "TimePeriod") -> dt.timedelta:
+    def intersection(self, other: Self) -> dt.timedelta:
         """
         Calculate the amount of time included in both periods.
         The duration includes start and end dates.
