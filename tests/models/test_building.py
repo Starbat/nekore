c96f60732c48779482ffe5a8021cf43ea84d68b1
Starbat <45294825+Starbat@users.noreply.github.com>
2024-03-23 01:47:02 +0100

    test: Building.apartment_of with wrong tenant

--- tests/models/test_building.py
+++ tests/models/test_building.py
@@ -1,6 +1,8 @@
 from decimal import Decimal
 from typing import Final
 
+import pytest
+
 from nekore.models import Apartment, Building, Tenant
 
 
@@ -30,3 +32,15 @@ def test_apartment_of(tenant_aurore_jones: Tenant, tenant_kim_jackson: Tenant) -
     )
     building: Final = Building(apartments=[aurores_apartment, kims_apartment])
     assert building.apartment_of(tenant_aurore_jones) == aurores_apartment
+
+
+def test_apartment_of_wrong_tenant(
+    tenant_aurore_jones: Tenant, tenant_kim_jackson: Tenant
+) -> None:
+    aurores_apartment: Final = Apartment(
+        tenants=[tenant_aurore_jones], floor_space=Decimal(100)
+    )
+    building: Final = Building(apartments=[aurores_apartment])
+
+    with pytest.raises(NotImplementedError):
+        building.apartment_of(tenant_kim_jackson)
