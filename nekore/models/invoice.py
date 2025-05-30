fb2dfe5f912dca4dee924ddf6c7bc599d7a9c8bf
Starbat <45294825+Starbat@users.noreply.github.com>
2025-05-30 16:36:48 +0200

    feat: make invoice.net_amount optional

--- nekore/models/invoice.py
+++ nekore/models/invoice.py
@@ -6,16 +6,18 @@ from decimal import Decimal
 @dataclass(frozen=True, slots=True)
 class Invoice:
     issuer_name: str
-    net_amount: Decimal
     date: dt.date
     gross_amount: Decimal
     privileged_amount: Decimal = field(default_factory=lambda: Decimal("0.00"))
+    net_amount: Decimal | None = None
     name: str | None = None
 
     def __post_init__(self) -> None:
         self._validate_gross_not_less_net()
 
     def _validate_gross_not_less_net(self) -> None:
+        if not self.net_amount:
+            return
         if (self.gross_amount < 0) != (self.net_amount < 0):
             raise ValueError("Net amount and gross amount must have the same sign.")
         if (abs(self.gross_amount) - abs(self.net_amount)) < 0:
